8a4bc97be66ca9e9a40a24c0358cf0d87a791f1c
kris1pl kris1pl <kris1.pl@gmail.com>
2023-03-15 12:17:10 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -215,12 +215,15 @@ class Dev(Configuration):
     ACCOUNT_USERNAME_REQUIRED = False
     ACCOUNT_AUTHENTICATION_METHOD = "email"
 
-    REST_FRAMEWORK = {
+REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
-    ]
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+    ],
     }
 
 
