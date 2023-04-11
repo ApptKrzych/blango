79388caa04db388fa17c679aa5a7f825bf00d701
kris1pl kris1pl <kris1.pl@gmail.com>
2023-04-11 09:42:43 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,9 +1,27 @@
-const theNumber = 1
-let yourName = 'Ben'
-
-if (theNumber === 1) {
-  let yourName = 'Leo'
-  lert(yourName)
+for(let i = 0; i < 10; i+=1) {
+  console.log('for loop i: ' + i)
 }
 
-alert(yourName)
+let j = 0
+while(j<10) {
+  console.log('while loop j: ' + j)
+  j +=1 
+}
+
+let k = 10
+
+do {
+  console.log('do while k: ' + k)
+} while(k < 10)
+
+const numbers = [1,2,3,4,5,6,7,8,9]
+
+numbers.forEach((value => {
+  console.log('For each value ' + value)
+}))
+
+const double = numbers.map(value => value * 2)
+
+console.log('Doubled numbers')
+
+console.log(double)
