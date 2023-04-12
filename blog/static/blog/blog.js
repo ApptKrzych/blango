f088cdb65557b65f7595013b5228a5bd0829dfe9
kris1pl kris1pl <kris1.pl@gmail.com>
2023-04-12 08:04:06 +0000

    Finish JavaScript promises

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,48 +1,26 @@
-class Greeter {
-  constructor (name) {
-    this.name = name
-  }
-
-  getGreeting () {
-    if (this.name === undefined) {
-      return 'Hello, no name'
-    }
-    return 'Hello, ' + this.name
-  }
-
-  showGreeting (greetingMessage) {
-    console.log(greetingMessage)
-  }
-
-  greet () {
-    this.showGreeting(this.getGreeting())
-  }
+function resolvedCallback(data) {
+  console.log('Resolved with data ' +  data)
 }
 
-// const g = new Greeter('John')
-// g.greet()
-
-
-class DelayedGreeter extends Greeter {
-  delay = 2000
-  constructor (name, delay) {
-    super(name)
-    if (delay !== undefined) {
-      this.delay = delay
-    }
-  }
-
-  greet () {
-    setTimeout(
-      () => {
-        this.showGreeting(this.getGreeting())
-      }, this.delay
-    )
-  }
+function rejectedCallback(message) {
+  console.log('Rejected with message ' + message)
 }
 
-const dg2 = new DelayedGreeter('Jonny 2 Seconds')
-dg2.greet()
+const lazyAdd = function (a, b) {
+  const doAdd = (resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("a and b must both be numbers")
+    } else {
+      const sum = a + b
+      resolve(sum)
+    }
+  }
+  return new Promise(doAdd)
+}
 
-const dg1 = new DelayedGreeter('Jonny 1 Second', 1000)
-dg1.greet()
+
+const p = lazyAdd(3, 4)
+
+p.then(resolvedCallback, rejectedCallback)
+
+lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
